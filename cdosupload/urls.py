ae9e8d30ab502d88a4618b81e6846e3eabaa5f7d
Dr. Dos <doctordos@gmail.com>
2025-05-19 22:40:15 -0700

    OG Images

--- cdosupload/urls.py
+++ cdosupload/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 from .views import *
 
 urlpatterns = [
-    path("", Directory_Listing_View.as_view())
+    path("", Directory_Listing_View.as_view()),
+    path("edit", Edit_Text_View.as_view()),
 ]
