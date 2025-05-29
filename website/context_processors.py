ba5bf39f35c8461528553ae6a5c1307fb1b4aa4a
Dr. Dos <doctordos@gmail.com>
2025-05-28 23:03:30 -0700

    Page BG changes daily

--- website/context_processors.py
+++ website/context_processors.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 from django.conf import settings
 
 from random import choice
@@ -10,12 +11,25 @@ def pokyfriends_global_context(request):
     context["TS"] = BOOT_TS
     context["DEBUG"] = settings.DEBUG
     #TODO glob these on server boot
-    bg = choice([
+    bgs = [
         "cs-arthur.png",
         "cs-night.png",
         "kdc-1.png",
         "kdc-2.png",
         "kdc-3.png",
-    ])
+        "bb-1.png",
+        "sor2-1.png",
+    ]
+    if not request.GET.get("bg"):
+        day_count = datetime.now().strftime("%j")
+        idx = int(day_count) % len(bgs)
+    else:
+        try:
+            idx = int(request.GET.get("bg"))
+        except ValueError:
+            idx = 0
+        if idx >= len(bgs):
+            idx = 1
+    bg = bgs[idx]
     context["bg_url"] = "/static/global/bg/" + bg
     return context
