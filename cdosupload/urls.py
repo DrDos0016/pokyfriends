ef3c949b470f61c05c3530ad8bb7f16b1bcedaa2
Dr. Dos <doctordos@gmail.com>
2026-03-09 22:51:35 -0700

    Add url name

--- cdosupload/urls.py
+++ cdosupload/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 from .views import *
 
 urlpatterns = [
-    path("", Directory_Listing_View.as_view()),
+    path("", Directory_Listing_View.as_view(), name="cdosupload"),
     path("edit", Edit_Text_View.as_view()),
 ]
